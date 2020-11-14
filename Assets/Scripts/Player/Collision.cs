ec949cc9a81646b27c1a93e2944aaac9737b0ac4
Exyde <54008677+Exyde@users.noreply.github.com>
2020-11-14 13:22:36 +0100

    Player Movement
    First commit.
    Added side move, jump, wall jump
    Class for collisions
    Class for better Jumping
    Class for movement
    
    Need to fix jump/wall behaviors

---
+++ Assets/Scripts/Player/Collision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+    //Class for handling side and bottom collisions.
+    //You have to tweak the Offsets Vector2 to get it working properly.
+
+    [Header("Layers")]
+    public LayerMask groundLayer;
+
+    [Space]
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    public int wallSide;
+
+    [Header("Collision")]
+    public float collisionRadius = .25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.red;
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer)
+              || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+
+        wallSide = onRightWall ? -1 : 1;
+    }
+
+	private void OnDrawGizmos()
+	{
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Collision coll;
+
+    [Space]
+
+    [Header ("Stats")]
+    public float speed = 10;
+    public float jumpForce = 5;
+    public float slideSpeed = 5;
+    public float wallJumpLerp = 10;
+    public float dashSpeed = 20;
+
+
+    [Space]
+    [Header("Booleans")]
+    public bool canMove;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
+
+    [Space] 
+    private bool groundTouch;
+    private bool hasDashed;
+    //Facing side
+    public int side = 1;
+
+    void Start()
+    {
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+
+        //Walk
+        Walk(dir);
+
+        //Jumps
+        if (Input.GetButtonDown("Jump"))
+        {
+            //anim.SetTrigger("jump");
+
+            if (coll.onGround) {
+                Jump(Vector2.up, false);
+            }
+            if (coll.onWall && !coll.onGround)
+			{
+                WallJump();
+            }
+        }
+
+        if (coll.onGround && !isDashing)
+        {
+            wallJumped = false;
+            GetComponent<BetterJumping>().enabled = true;
+        }
+
+        if (x > 0)
+        {
+            side = 1;
+            //anim.Flip(side);
+        }
+        if (x < 0)
+        {
+            side = -1;
+           //anim.Flip(side);
+        }
+
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        if (!canMove)
+            return;
+
+        if (wallGrab)
+            return;
+
+        if (!wallJumped)
+        {
+            rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+        }
+    }
+
+    private void Jump(Vector2 dir, bool wall)
+    {
+        //slideParticle.transform.parent.localScale = new Vector3(ParticleSide(), 1, 1);
+        //ParticleSystem particle = wall ? wallJumpParticle : jumpParticle;
+
+        //Can adjust the force if wallAttached
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
+
+        //particle.Play();
+    }
+
+    private void WallJump()
+	{
+        if ((side == 1 && coll.onRightWall) || side == -1 && !coll.onRightWall)
+		{
+            side *= -1;
+            //anim.Flip(side);
+		}
+
+        //StopCoroutine(DisableMovement(0));
+        //StartCoroutine(DisableMovement(.1f));
+
+        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+        Jump((Vector2.up / 1.5f + wallDir / 1.5f), true);
+        wallJumped = true;
+    }
+}
