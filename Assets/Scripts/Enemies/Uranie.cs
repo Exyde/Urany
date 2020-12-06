0f1838362827771471886a666e2dcce076504e06
Exyde <54008677+Exyde@users.noreply.github.com>
2020-12-06 17:00:52 +0100

    Urany Prefab

--- Assets/Scripts/Enemies/Uranie.cs
+++ Assets/Scripts/Enemies/Uranie.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class Uranie : MonoBehaviour
 {
+    [Header ("Components")]
     public Rigidbody2D rb;
     public SpriteRenderer sr;
+    public Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-    }
-
-    void Update()
-    {
-        
+        anim = GetComponent<Animator>();
     }
 }
